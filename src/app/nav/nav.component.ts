7261154064b8f9cbbec81ee74e41bfdb79dded22
piero lavado cervantes <piero_lvdo_c@hotmail.com>
2024-02-05 18:20:49 -0500

    end of section 18

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -20,6 +20,7 @@ export class NavComponent implements OnInit{
     this.accountService.login(this.model).subscribe({
       next: response => {
         this.router.navigateByUrl('/members');
+        this.model = {};
       },
     });
   }
